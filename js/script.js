3bf93bf67aebc1d02a5a17ebc785df9dc1c8a80c
Santosh <kasantosh@yahoo.com>
2021-01-30 21:52:25 -0500

    team slider implemented partially

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ $(window).on('load', function() {
   $('#status').fadeOut();
   $('#preloader').delay(350).fadeOut('slow');
 });
+
+/*===================================
+            Team 
+======================================*/
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 500,
+    margin: 10,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    dots: false
+  });
+});
